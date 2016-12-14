0ce5160f0b333c32ef851e2afdfe3a08a4df7645
Rachelle <archaurore@gmail.com>
2016-12-14 17:11:10 -0500

    added morgan and a server listener

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const {BlogPosts} = require('./models');
 
 const app = express();
+
+app.use(morgan('common'));
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log(`Your app is running on port ${process.env.PORT || 8080}`);
+});
