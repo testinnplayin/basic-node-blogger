6d09843d52b9217c3d6de03b02f662f85b6605ec
Rachelle <archaurore@gmail.com>
2016-12-14 17:43:51 -0500

    added get and post to blogRouter file

--- blogRouter.js
+++ blogRouter.js
@@ -14,4 +14,29 @@ const BlogPosts = require('./models');
 BlogPosts.create('Node is fun', 'Seriously, it really is!', 'R Wood', 'Today');
 BlogPosts.create('The sky is falling on our heads', 'And I have a menhir or two to sell.', 'Obelix', 'Gallo-Roman times');
 
+//CRUD functions
+
+router.get('/', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'],
+		lng = requiredFields.length;
+
+	for (let i = 0; i < lng; i++) {
+		const field = requiredFields[i];
+
+		if (!(field in req.body)) {
+			const msg = `Missing \'${field}\'. Please enter.`;
+			console.error(msg);
+
+			return res.status(400).send(msg);
+		}
+	}
+
+	const blog = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.bodt.publishDate);
+	res.status(200).json(blog);
+});
+
 module.exports = router;
