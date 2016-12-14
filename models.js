d0f8cb629afcf84a64bf40f5df45abcb962130e2
Rachelle <archaurore@gmail.com>
2016-12-14 16:52:33 -0500

    added a blog post model

---
+++ models.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const uuid = require('uuid');
+
+//temporarily stored blog posts
+
+function StorageException(message) {
+	this.message = message;
+	this.name = "StorageException";
+}
+
+const BlogPosts = {
+	create: function(title, content, author, publishDate) {
+		const post = {
+			id: uuid.v4(),
+			title: title,
+			content: content,
+			author: author,
+			publishDate: publishDate || Date.now()
+		};
+		this.posts.push(post);
+		return post;
+	},
+	get: function(id=null) {
+		if (id !== null) { //return desired post otherwise all posts
+			return this.posts.find(post => post.id === id);
+		}
+		//returns posts sorted by descending publish date
+		return this.posts.sort(function(a, b) {
+			return b.publishDate - a.publishDate;
+		});
+	},
+	delete: function(id) {
+		const postIndex = this.posts.findIndex(post => post.id === id);
+		if (postIndex > -1) {
+			this.posts.splice(postIndex, 1);
+		}
+	},
+	update: function(updatedPost) {
+		const {id} = updatedPost;
+		const postIndex = this.posts.findIndex(post => post.id === id);
+		if (postIndex === -1) {
+			throw StorageException(`Can't update item \'${id}\' because doesn't exist.`);
+		}
+		this.posts[postIndex] = Object.assign(this.posts[postIndex], updatedPost);
+		return this.posts[postIndex];
+	}
+};
+
+function createBlogPostsModel() {
+	const storage = Object.create(BlogPosts);
+	storage.posts = [];
+	return storage;
+}
+
+module.exports = {BlogPosts: createBlogPostsModel()};
