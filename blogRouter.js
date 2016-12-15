d51e07f0e460ced57dc000ef3b561a4a268f430f
Rachelle <archaurore@gmail.com>
2016-12-15 14:19:20 -0500

    added delete and put

--- blogRouter.js
+++ blogRouter.js
@@ -40,4 +40,41 @@ router.post('/', jsonParser, (req, res) => {
 	res.status(200).json(blog);
 });
 
+router.delete('/:id', (req, res) => {
+	BlogPosts.Posts.delete(req.params.id);
+	console.log(`Deleted blog post post \'${req.params.id}\'.`);
+	res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'],
+		lng = requiredFields.length;
+
+	for(let i = 0; i < lng; i++) {
+		const field = requiredFields[i];
+
+		if (!(field in req.body)) {
+			const msg = `Missing \'${field}\'. Please enter before proceeding.`;
+			console.error(msg);
+			return res.status(400).send(msg);
+		}
+	}
+
+	if (req.params.id !== req.body.id) {
+		const msg = (
+			`Please enter a valid id because id entered (${req.params.id}) does not match any blog post id.`);
+		console.error(msg);
+		return res.status(400).send(msg);
+	}
+	console.log(`Updating blog post post \'${req.params.id}\'.`);
+	const updatedPost = BlogPosts.Posts.update({
+		id: req.params.id,
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author,
+		publishDate: req.body.publishDate
+	});
+	res.status(204).json(updatedPost);
+});
+
 module.exports = router;
