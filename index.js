530a6ff02f5f8dc05e4ea7a22a497beabfef0390
Rachelle <archaurore@gmail.com>
2016-12-19 15:02:16 -0500

    added a test to GET

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-	console.log(`Your app is running on port ${process.env.PORT || 8080}`);
-});
+//new server runServer function replaces the old app.listen function
+
+let server;
+
+function runServer() {
+	const port = process.env.PORT || 8080;
+
+	return new Promise((resolve, reject) => {
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on port ${port}.`);
+			resolve(server);
+		}).on('error', err => {
+			reject(err);
+		});
+	});
+}
+
+function closeServer() {
+	return new Promise ((resolve, reject) => {
+		console.log(`Closing server.`);
+		server.close(err => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.log(err));
+}
+
+module.exports = {app, runServer, closeServer};
--- test/test-index.js
+++ test/test-index.js
@@ -1,3 +1,36 @@
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
+const {app, runServer, closeServer} = require('../index');
+
+const should = chai.should();
+
+chai.use(chaiHTTP);
+
+describe('BlogPosts', function() {
+	before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+
+	it('should list blog posts on GET', function() {
+		return chai.request(app)
+			.get('/blog-posts')
+			.then(function(res) {
+				res.should.be.json;
+				res.body.should.be.a('array');
+				res.body.length.should.be.at.least(1);
+
+				const expectedKeys = ['id', 'title', 'content', 'author'];
+				res.body.forEach(function(item) {
+					item.should.be.a('object');
+					item.should.include.keys(expectedKeys);
+				});
+			});
+	});
+
+	it();
+});
