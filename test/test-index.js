45e3cb45c73754573cad4a8ebd7fb7beb1940b76
Rachelle <archaurore@gmail.com>
2016-12-19 15:17:41 -0500

    added test for POST

--- test/test-index.js
+++ test/test-index.js
@@ -32,5 +32,20 @@ describe('BlogPosts', function() {
 			});
 	});
 
-	//it();
+	it('should create a new blog post on POST', function() {
+		const newPost = {title: 'More Menhirs on Sale', content: 'This is a one-off sale on last season\'s menhirs.', author: 'Obelix', publishDate: 'Gallo-Roman times'};
+		return chai.request(app)
+			.post('/blog-posts')
+			.send(newPost)
+			.then(function(res) {
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.include.keys('id', 'title', 'content', 'author');
+				res.body.id.should.not.be.null;
+				res.body.should.deep.equal(Object.assign(newPost, {id: res.body.id}));
+
+			});
+	});
+
 });
